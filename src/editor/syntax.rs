57d8f8221c19f287bc1ce461bde900bd96768b03
FelixMo42 <felix@tacocat.com>
2021-10-09 13:25:06 -0600

    Got synatx hightlighting with logos to work

--- src/editor/syntax.rs
+++ src/editor/syntax.rs
@@ -1,16 +1,60 @@
+use logos::{Lexer, Logos};
+
+pub const BLUE: &str = "\x1b[36m";
 pub const GREEN: &str = "\x1b[32m";
+pub const PURPLE: &str = "\x1b[38;2;208;202;140m";
+// pub const GRAY: &str = "\x1b[38;2;168;153;132m";
+pub const PINK: &str = "\x1b[38;2;0;200;200m";
 
 pub const RESET: &str = "\x1b[0m";
 
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+    #[token("mod")]
+    #[token("use")]
+    #[token("pub")]
+    #[token("fn")]
+    #[token("struct")]
+    #[token("enum")]
+    #[token("const")]
+    #[token("let")]
+    #[token("if")]
+    #[token("else")]
+    #[token("match")]
+    #[token("return")]
+    #[token("impl")]
+    KeyWord,
+
+    #[regex("[A-Z][a-zA-Z0-9_]*")]
+    #[token("usize")]
+    #[token("bool")]
+    Type,
+
+    #[regex("[a-z][a-zA-Z0-9_]*[.:]")]
+    Path,
+
+    #[regex("[a-z][a-zA-Z0-9_]*")]
+    Variable,
+
+    #[regex(r"[-]?[0-9]+[.]?[0-9]?")]
+    Number,
+
+    #[regex("[^a-zA-Z0-9_]")]
+    Punctuation,
+
+    #[error]
+    Error,
+}
+
 pub struct Synax<'a> {
-    // text: &'a str,
-    split: Vec<&'a str>,
+    tokens: Lexer<'a, Token>,
 }
 
 impl<'a> Synax<'a> {
     pub fn new(text: &'a str) -> Self {
+        let a = Token::lexer(text);
         return Synax {
-            split: text.split_inclusive(' ').rev().collect(),
+            tokens: Token::lexer(text),
         };
     }
 }
@@ -19,12 +63,18 @@ impl<'a> Iterator for Synax<'a> {
     type Item = (&'static str, &'a str);
 
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        if let Some(word) = self.split.pop() {
-            match word {
-                "mod " | "impl " | "use " | "fn " | "let " | "pub " | "else " | "if "
-                | "return " | "struct " => Some((GREEN, word)),
-                word => Some((RESET, word)),
-            }
+        if let Some(token) = self.tokens.next() {
+            let style = match token {
+                Token::Path => RESET,
+                Token::Variable => PURPLE,
+                Token::KeyWord => GREEN,
+                Token::Number => BLUE,
+                Token::Punctuation => RESET,
+                Token::Type => PINK,
+                Token::Error => RESET,
+            };
+
+            Some((style, self.tokens.slice()))
         } else {
             None
         }
